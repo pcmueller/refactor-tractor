5676590993dd9c958d3986fdcf830945c4122377
pcmueller <pmuellerleile@gmail.com>
2021-04-13 19:53:00 -0600

    Construct Ingredient class and testing

--- src/recipe.js
+++ src/recipe.js
@@ -1,3 +1,5 @@
+const Ingredient = require("./Ingredient");
+
 class Recipe {
   constructor(recipe) {
     this.id = recipe.id;
@@ -5,11 +7,26 @@ class Recipe {
     this.image = recipe.image;
     this.tags = recipe.tags;
     this.ingredients = recipe.ingredients;
+    this.costedIngredients = [];
   }
-  calculateIngredientsCost() {
-    // return this.ingredients.map(i => {
-    //   ingredientData.find(ingredient => ingredient === i);
-    // });
+
+  retrieveIngredientPricing(data) {
+    this.ingredients.forEach(ingredient => {
+      return data.forEach(datum => {
+        if (ingredient.id === datum.id) {
+          this.costedIngredients.push(new Ingredient(ingredient, datum.estimatedCostInCents));
+        }
+      });
+    });
+  }
+
+  calculateIngredientsCost(data) {
+    this.retrieveIngredientPricing(data);
+    const cents = this.costedIngredients.reduce((total, item) => {
+      total += item.costInCents * item.quantity.amount;
+      return total;
+    }, 0);
+    return parseFloat((cents/100).toFixed(2));
   }
 }
 
